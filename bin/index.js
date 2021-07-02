f7818ed46c5f1d863f173c004c15d5dee6778bbc
kanweiwei <kwwnjujlc@sina.com>
2021-07-02 15:54:34 +0800

    chore: ora

--- bin/index.js
+++ bin/index.js
@@ -4,42 +4,43 @@ const fs = require("fs");
 const process = require("process");
 const http = require("http");
 const codegen = require("../lib/index.js");
+const ora = require("ora");
 
 const envFileName = "swagger-codegen.config.js";
 const envFilePath = path.join(process.cwd(), envFileName);
 if (fs.existsSync(envFilePath)) {
   const data = require(envFilePath);
   const cacheFile = path.join(process.cwd(), ".swagger-cache");
-  if(fs.existsSync(cacheFile)) {
-    console.log('cache file exists\n');
+  if (fs.existsSync(cacheFile)) {
+    ora("cache file exists").info();
     fs.readFile(cacheFile, (err, t) => {
-      if(err){
-        console.log(err.message + '\n');
+      if (err) {
+        console.log(err.message + "\n");
         process.exit(0);
       }
-      codegen(JSON.parse(t.toString('utf-8')), data);
-    })
+      codegen(JSON.parse(t.toString("utf-8")), data);
+    });
   } else {
     const url = data.url;
     let swaggerData = "";
-    console.log('start request swagger.json\n')
+    const spinner = ora("request swagger.json").start();
     const client = http.get(url, (res) => {
       res.on("data", (c) => {
         swaggerData += c;
       });
       res.on("end", () => {
-        console.log('end request swagger.json\n')
+        spinner.succeed();
         const swaggerJson = JSON.parse(swaggerData);
         fs.writeFileSync(
           path.join(process.cwd(), ".swagger-cache"),
           swaggerData,
           { encoding: "utf-8" }
         );
+        ora('generate .swagger-cache').succeed()
         codegen(swaggerJson, data);
       });
     });
   }
-  
 } else {
   console.error("can't find the swagger-codegen.config.js file");
   process.exit(0);
